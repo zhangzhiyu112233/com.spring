3c911dcb6ef883a6b7fd733c29877bd4b7f9ce90
zhangzhiyu112233 <13820233617@163.com>
2020-03-26 13:25:34 +0800

    Initial commit

---
+++ src/test/java/SpringTest.java
@@ -0,0 +1,12 @@
+import org.junit.Test;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class SpringTest {
+    @Test
+    public void testSpring(){
+        ApplicationContext app=new ClassPathXmlApplicationContext();
+
+    }
+
+}
